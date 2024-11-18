f6858c9f461466b5007ca444ccec2e04c1e2702e
Felipe Lemos <felipelib235@gmail.com>
2024-11-18 12:40:54 -0300

    Update SQL_Scripts.sql

--- SQL_Scripts.sql
+++ SQL_Scripts.sql
@@ -3,7 +3,9 @@ SELECT
  ROUND(AVG(nota_candidato), 2) nota_media, 
  ROUND(AVG(nota_corte), 2) nota_corte_media,
  ROUND(AVG(nota_corte), 2) - ROUND(AVG(nota_candidato), 2) as diferenca_notas, 
- ROUND(AVG(nota_ch), 2) media_humanas, ROUND(AVG(nota_cn), 2) media_naturezas,
+ ROUND(AVG(nota_l), 2) media_linguagens,
+ ROUND(AVG(nota_ch), 2) media_humanas,
+ ROUND(AVG(nota_cn), 2) media_naturezas,
  ROUND(AVG(nota_m), 2) media_matematica,
  ROUND(AVG(nota_r), 2) media_redacao, 
  ROUND(COUNT(CASE WHEN status_aprovado is true THEN 1 END)/COUNT(status_aprovado) * 100, 2) taxa_aprovacao
@@ -15,6 +17,7 @@ SELECT
  ROUND(AVG(nota_candidato), 2) nota_media,
  ROUND(AVG(nota_corte), 2) nota_corte_media,
  ROUND( ROUND(AVG(nota_corte), 2) - ROUND(AVG(nota_candidato), 2), 2) as diferenca_notas,
+ ROUND(AVG(nota_l), 2) media_linguagens,
  ROUND(AVG(nota_ch), 2) media_humanas,
  ROUND(AVG(nota_cn), 2) media_naturezas,
  ROUND(AVG(nota_m), 2) media_matematica,
@@ -37,6 +40,7 @@ GROUP BY estado;
 SELECT 
  sexo, 
  COUNT(sexo) contagem, 
+ ROUND(AVG(nota_l), 2) media_linguagens,
  ROUND(AVG(nota_ch), 2) media_humanas,
  ROUND(AVG(nota_cn), 2) media_naturezas,
  ROUND(AVG(nota_m), 2) media_matematica,
